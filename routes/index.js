74d7569dee5eef1a64456a114df23db9e4995cdc
Chase <2chasehays@gmail.com>
2025-03-20 21:06:14 -0500

    Added splashpage.ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('pages/home', { 
+  res.render('pages/splashpage.ejs', { 
     title: 'Chase Cooking',
     description: "Chef Gordon Ramsey won't know what hit him!"
   });
