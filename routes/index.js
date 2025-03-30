603887654d3b6c757384cba1d2b3888bde79b692
henryhafner <hhafner@trinity.edu>
2025-03-30 11:17:26 -0500

    nav stuff

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,6 @@ router.get('/signup', (req, res) => {
   res.render('pages/signup');
 })
 
-router.get('/home', (req, res) => {
-  res.render('pages/home', { 
-    //THIS WILL EVENTUALLY BE DATA FROM THE DB, THIS IS A PLACEHOLDER OBJ
-    leaderboardPlayers: [
-      {name: "paul_GOAT", score: 1500},
-      {name: "jack_GOAT", score: 1499},
-      {name: "melissa_GOAT", score: 1493},
-      {name: "harold_from_accounting", score: 5}
-    ]
-  });
-});
+
 
 module.exports = router;
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -12,4 +12,15 @@ router.get('/', (req, res) =>{
     });
 });
 
+router.get('/to_home',(req,res) =>{
+    res.render('pages/home', {
+        leaderboardPlayers: [
+            {name: "paul_GOAT", score: 1500},
+            {name: "jack_GOAT", score: 1499},
+            {name: "melissa_GOAT", score: 1493},
+            {name: "harold_from_accounting", score: 5}
+          ]
+    });
+})
+
 module.exports = router;
