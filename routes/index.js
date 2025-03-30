6053bb6ea7caecf7e445c950c0cfe4ddb0433a0c
henryhafner <hhafner@trinity.edu>
2025-03-30 15:26:26 -0500

    added login and signup functionality

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const connection = require('../database');
 
 router.get('/', (req, res) => {
   res.render('pages/login');
 });
 
-router.get('/signup', (req, res) => {
+router.get('/register', (req, res) =>{
   res.render('pages/signup');
 })
+
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  //Checks if user has an account
+  let sql = 'SELECT * FROM user_information WHERE username=?';
+  connection.query(sql, [username], async (err, result) => {
+      if (err) throw err;
+      if (result.length === 0 ) {
+          return res.status(401).send('Invalid username');
+      }
+      const isValid = await bcrypt.compare(password, result[0].hash_password); 
+      if(!isValid){
+          return res.status(401).send('Invalid password');
+      }
+      req.session.user = result[0];
+      req.session.save();
+      res.render('pages/home');
+  });
+
+});
+
+
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 13);
+  //Checks if username is taken
+  console.log(username);
+  console.log(password);
+  console.log(hash);
+  let checkUser = 'SELECT * FROM user_information WHERE username=?';
+  connection.query(checkUser, [username], (err, result) => {
+      if (err) throw err;
+      if (result.length != 0) {
+          return res.status(401).send('Username is already taken');
+      }
+      console.log('hello');
+
+      //Adds user to database
+      let sql = 'INSERT INTO user_information(username, hash_password) VALUES(?,?)';
+      connection.query(sql, [username, hash], (err, result) => {
+          if (err) throw err;
+          const getUser = result.insertId;
+          //Querying user that was just added
+          let sql2 = 'SELECT * FROM user_information WHERE username=?';
+          connection.query(sql2, [getUser], (err, result) => {
+              if (err) throw err;
+              req.session.user = result[0];
+              req.session.save();
+              res.redirect('/');
+          });
+      });
+  });
+});
 
 router.get('/home', (req, res) => {
   res.render('pages/home', { 
